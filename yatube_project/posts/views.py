b454abdbe1f22e7da35e35d9bb299a8432ec1f6e
sofi <@sofiiila>
2024-06-20 20:57:27 +0300

    ссылки в шаблонах

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -4,9 +4,14 @@ from django.shortcuts import render
 def index(request):
     template = 'posts/index.html'
     title = 'Здравствуй, Рус! Гойда!'
+    li = ['Пельмени', 'Борщ']
+    p = 'Пельмени – знаменитое блюдо русской кухни, имеющее древние китайские, финно-угорские, тюркские и славянские корни.Современное название происходит от удмуртского «пельнянь» – «хлебное ухо». Аналоги пельменей существуют во многих кухнях мира.Вкус, сытность и удобство хранения сделали пельмени исключительно популярными,готовые пельмени можно купить в любом продуктовом магазине.'
+    slug = 'group_posts'
     context = {
         'title': title,
-        'text': 'Это главная страница проекта Yatube',
+        'li': li,
+        'p': p,
+        'slug': slug,
     }
     return render(request, template, context)
 
@@ -14,6 +19,6 @@ def index(request):
 def group_posts(request, slug):
     context = {
         'slug': slug,
-        'text': 'Здесь будет информация о группах проекта Yatube'
+        'text': 'Здесь будет информация о группах проекта Yatube',
     }
     return render(request, 'posts/group_posts.html', context)

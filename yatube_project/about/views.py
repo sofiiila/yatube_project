f78fc439595b7d2a5f60f10a8f2cff7d14e2f055
sofi <@sofiiila>
2024-08-21 19:13:09 +0300

    приложение about

---
+++ yatube_project/about/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+class AboutAuthorView(TemplateView):
+    template_name = 'about/author.html'
+
+
+class AboutTechView(TemplateView):
+    template_name = 'about/tech.html'

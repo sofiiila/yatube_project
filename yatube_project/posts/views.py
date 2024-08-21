b5780303c2a6bb4faa7e324e5f346b610f9d3dbe
sofi <@sofiiila>
2024-08-21 18:39:54 +0300

    pagination

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 import datetime
 
 from .models import Post, Group, User
 
 
+
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.all().order_by('-pub_date')
+
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 

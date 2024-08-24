22bc08e1d014a23df74f0ed0ed67f4c47b32b3f2
sofi <@sofiiila>
2024-08-24 23:52:03 +0300

    /create/ полностью как нужно было сделать

--- yatube_project/posts/forms.py
+++ yatube_project/posts/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from .models import Post
 
 
@@ -7,3 +6,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['text', 'group']
+        labels = {
+            'text': 'Текст поста',
+            'group': 'Группа',
+        }
+        help_texts = {
+            'text': 'текст нового поста',
+            'group': 'группа, к которой относится пост',
+        }

887a502eb61b75420e44d7304d157b6b5fb954d1
sofi <@sofiiila>
2024-09-09 20:30:55 +0300

    тестирование передаваемых шаблонов вьюшки статичных страниц

--- yatube_project/about/tests.py
+++ yatube_project/about/tests.py
@@ -1,12 +1,13 @@
 from http import HTTPStatus
 from django.test import TestCase, Client
+from django.urls import reverse
 
 
-class StaticPagesUrlTests(TestCase):
+class StaticPagesTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
 
-    def test_about_urls_exists_at_desired_location(self):
+    def test_about_tech_urls_exists_at_desired_location(self):
         """Проверка доступности адресов /about/author/ и /about/tech/."""
         urls = ['/about/author/', '/about/tech/']
 
@@ -14,3 +15,15 @@ class StaticPagesUrlTests(TestCase):
             with self.subTest(url=url):
                 response = self.guest_client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_about_tech_pages_uses_correct_template(self):
+        urls_templates = {
+            'about:author': 'about/author.html',
+            'about:tech': 'about/tech.html'
+        }
+
+        for url, template in urls_templates.items():
+            with self.subTest(url=url):
+                response = self.guest_client.get(reverse(url))
+                self.assertTemplateUsed(response, template)
+
